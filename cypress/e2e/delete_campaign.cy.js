f70150b8cabb789944844b0ffb50e04770eed5fb
Juan Lozano <juaniloza83@gmail.com>
2023-03-08 10:04:54 -0300

    Finish delete campaign

--- cypress/e2e/delete_campaign.cy.js
+++ cypress/e2e/delete_campaign.cy.js
@@ -1,5 +1,5 @@
 /// <reference types="Cypress" />
-import { page_qa, password, password_admin, password$, email_company, login_as, pass_intro, home, toolbox_compa_jobs_page, campaign_page, name_row, name_job} from "./variables_naw.js";
+import { page_qa, password, password_admin, password$, email_company, login_as, pass_intro, home, toolbox_compa_jobs_page, campaign_page, name_row, name_job, widget_credit} from "./variables_naw.js";
 
 describe('Borrar campaña', () => {
     before(function () {
@@ -15,7 +15,7 @@ describe('Borrar campaña', () => {
         });
       });
 
-    xit('Check datos del modal y cierre al clickear fuera en tabla campañas', function() {
+    it('Check datos del modal y cierre al clickear fuera en tabla campañas', function() {
         cy.visit(campaign_page).wait(2000);
         cy.get('.name-field').first().children().eq(0).invoke('text').as('nameCampaignTable');
         cy.get('.no-border').first().click().wait(500);
@@ -30,7 +30,7 @@ describe('Borrar campaña', () => {
 
         
     });  
-    xit('Check datos del modal y cierre al clickear fuera en Detalles campañas', function() {
+    it('Check datos del modal y cierre al clickear fuera en Detalles campañas', function() {
         cy.visit(campaign_page).wait(2000);
         avoidConsoleError();
 
@@ -52,11 +52,11 @@ describe('Borrar campaña', () => {
             cy.get('body').should('not.have.class', 'modal-open');
     });  
 
-    xit('Eliminar desde tabla y chequear si se borro', function() {
+    it('Eliminar desde tabla y chequear si se borro', function() {
         cy.visit(campaign_page).wait(2000);
         //Creo una campaña
         cy.get('.card-header > .button').click().wait(1500);
-        cy.get('input').eq(0).type(name_row).invoke('val').as('nameDeleteCampaign').wait(1000);
+        cy.get('input').eq(0).type(name_row+'c1').invoke('val').as('nameDeleteCampaign').wait(1000);
         cy.get('input').eq(1).type(1).wait(1000);
         cy.get('.button-group > .button').click().wait(2500);
         
@@ -70,7 +70,7 @@ describe('Borrar campaña', () => {
         cy.get('.snackbar').children().should('contain.text', 'Campaña eliminada');
     });
 
-    xit('Eliminar desde Detalles y chequear si se borro', function() {
+    it('Eliminar desde Detalles y chequear si se borro', function() {
         cy.visit(campaign_page).wait(2000);
         Cypress.on('uncaught:exception', (err, runnable) => { // es para borrar los errores de consola
           // returning false here prevents Cypress from
@@ -79,7 +79,7 @@ describe('Borrar campaña', () => {
         });
         //Creo una campaña
         cy.get('.card-header > .button').click().wait(1500);
-        cy.get('input').eq(0).type(name_row).invoke('val').as('nameDeleteCampaign').wait(1000);
+        cy.get('input').eq(0).type(name_row+'c2').invoke('val').as('nameDeleteCampaign').wait(1000);
         cy.get('input').eq(1).type(1).wait(1000);
         cy.get('.button-group > .button').click().wait(2500);
         
@@ -96,6 +96,64 @@ describe('Borrar campaña', () => {
         });
 
         cy.get('.snackbar').children().should('contain.text', 'Campaña eliminada');
+    });
+
+    it('Cuentas: Se devuelven los creditos a disponibles', function() {
+      avoidConsoleError();
+      cy.visit(campaign_page).wait(1500);
+      
+      //Creo una campaña
+      cy.get('.card-header > .button').click().wait(1500);
+      cy.get('input').eq(0).type(name_row).invoke('val').as('nameDeleteCampaign').wait(1000);
+      cy.get('input').eq(1).type(1).wait(1000);
+      cy.get('.button-group > .button').click().wait(2500);
+      
+      widget_credit(0, 'pre_avaiable');
+      cy.wait(1500);
+
+      cy.get('.number.centered-values.consumed').then(function (filterNumber) {
+        let result = Object.values(filterNumber).slice(0,10);
+        let jefe = result.filter(item => item.innerText == 0); //Filtro aquellos que sean iguales a 0 asi los puedo eliminar
+
+        cy.get(jefe).first().parent().parent().find('.centered-values.associated').invoke('text').as('numberAssociated')
+        cy.get(jefe).first().parent().parent().find('.no-border').click().wait(500)
+          .then(function (item) {
+            cy.contains('Eliminar').click({force: true}).wait(2000);
+            cy.get('.modal-footer > .primary').click();
+            cy.wait(3000);
+
+            cy.get('.credit-widget-body-container').eq(0).children().eq(1).then(function (result) {
+              expect(result).to.contain(Number(this.pre_avaiable) + Number(this.numberAssociated));
+            })
+          })
+
+
+      })
+    });
+
+    it('Boton disabled al eliminar alguien con creditos consumidos desde TABLA', function() {
+      cy.visit(campaign_page).wait(1500);
+
+      //Tomo los numeros  de la columna consumidos
+      cy.get('.number.centered-values.consumed').then(function (filterNumber) {
+        let result = Object.values(filterNumber).slice(0,10); //Tomo solo los numeros consumidos, el resto lo borro
+        let jefe = result.filter(item => item.innerText != 0); //Filtro solo aquellos que sean distintos a 0
+        
+        cy.get(jefe).first().parent().parent().find('.no-border').click().wait(500); //Desde el numero navego hacia atras hasta llegar al boton opciones
+        cy.contains('Eliminar').should('have.class', 'text-white'); //Osea esta disabled
+      })
+    });
+
+    it('Boton disabled al eliminar alguien con creditos consumidos desde DETALLES', function() {
+      cy.visit(campaign_page).wait(1500);
+      cy.get('.number.centered-values.consumed').then(function (filterNumber) {
+        let result = Object.values(filterNumber).slice(0,10);
+        let jefe = result.filter(item => item.innerText != 0);
+
+        cy.get(jefe).first().parent().parent().click().wait(1500); //Visito detalles de esa campaña
+        cy.get('label > .material-symbols-rounded').click();
+        cy.contains('Eliminar').should('be.disabled').wait(2000);
+      })
     });
 });
 
@@ -114,3 +172,6 @@ function avoidConsoleError() {
     return false
   });
 }
+
+
+// 08/03/22 10:04 funciono todo ok
